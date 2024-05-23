d1ac276bc2028ebef5f383de93256ca83c17721a
Gektor Gevorgyan <gektor.gevorgyan@ya.ru>
2024-05-23 13:40:47 +0400

    Removed additionals files(garbage)

--- assets/Beauty Concept CHSP Paris_files/index.js
+++
@@ -1,3 +0,0 @@
-const submeny = document.getElementsByClassName("category-btn")
-
-console.log(submeny[0].dataset["sub"])
--- assets/Beauty Concept CHSP Paris_files/navbar.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector('.hamburger-menu');
-const navMenu = document.querySelector('.menu');
- 
-hamburger.addEventListener('click', () => {
-  navMenu.classList.toggle('hide');
-});
