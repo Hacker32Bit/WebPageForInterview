9a40c9240e30f458189854575c3c2f29a15a7675
Gektor Gevorgyan <gektor.gevorgyan@ya.ru>
2024-05-25 17:27:55 +0400

    Added submenu for header menu

--- js/navbar.js
+++ js/navbar.js
@@ -4,13 +4,29 @@ const phone = document.querySelector('.phone')
 const search = document.querySelector('.search-btn');
 const searchCloseIcon = document.querySelector('.search-icon')
 const searchInput = document.querySelector('.header-search-input')
+const dropdown = document.querySelector(".dropdown")
+const submenu = document.querySelector(".submenu")
+
+window.addEventListener('click', (e) => {
+  // console.log(e.target)
+  
+  // Check if clicked on submenu
+  if (submenu.contains(e.target)) {
+    return
+  } // Check if clicked on menu button 
+  else if (dropdown.contains(e.target)) {
+    submenu.classList.toggle("closed")
+  } // Clicked on another place.
+  else {
+    submenu.setAttribute("class", "submenu closed")
+  }
+});
 
 // Listener for menu button
 hamburger.addEventListener('click', () => {
   navMenu.classList.toggle('hide');
   phone.classList.toggle('hide')
 });
-
 
 // Listener for search button click
 search.addEventListener('click', () => {
